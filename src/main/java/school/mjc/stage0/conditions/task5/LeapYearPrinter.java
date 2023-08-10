4e28d7de38682f25e5b656d4bda15d5b5f352c4a
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-10 18:37:00 +0400

    condition switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = false;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            isLeap = true;
+        }
+
+        switch (isLeap ? "leap" : "not leap") {
+            case "leap":
+                System.out.println("leap");
+                break;
+            case "not leap":
+                System.out.println("not leap");
+                break;
+        }
     }
 }
